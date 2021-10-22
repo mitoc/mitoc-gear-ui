f1b1fcd5cd31ed8bcb009357d0d58ffa23dac876
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-10-22 00:59:00 -0400

    Handle CSRF refresh

--- src/apiClient/client.ts
+++ src/apiClient/client.ts
@@ -2,7 +2,12 @@ type Data = { [key: string]: any };
 
 const API_HOST = "http://127.0.0.1:8000/api/v1";
 
-export async function request(path: string, method: string, data?: Data) {
+export async function request(
+  path: string,
+  method: string,
+  data?: Data,
+  maxRetry: number = 3
+) {
   const response = await fetch(`${API_HOST}${path}`, {
     method: method,
     headers:
@@ -15,6 +20,10 @@ export async function request(path: string, method: string, data?: Data) {
     credentials: "include",
     ...(data != null && method === "POST" && { body: JSON.stringify(data) }),
   });
+  if (response.status === 403) {
+    await refreshCsrfToken();
+    return request(path, method, data, maxRetry - 1);
+  }
   if (!response.ok) {
     throw Error(await parseResponse(response));
   }
@@ -37,11 +46,15 @@ let _csrfToken: any = null;
 
 async function getCsrfToken() {
   if (_csrfToken == null) {
-    const response = await fetch(`${API_HOST}/auth/csrf/`, {
-      credentials: "include",
-    });
-    const data = await response.json();
-    _csrfToken = data.csrfToken;
+    refreshCsrfToken();
   }
   return _csrfToken;
+}
+
+async function refreshCsrfToken() {
+  const response = await fetch(`${API_HOST}/auth/csrf/`, {
+    credentials: "include",
+  });
+  const data = await response.json();
+  _csrfToken = data.csrfToken;
 }
