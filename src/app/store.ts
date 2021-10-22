4596877bc8835bb359f8d54ef9af73da0060876b
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-10-22 17:19:35 -0400

    Remove Redux example

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 
-import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer,
   },
 });
