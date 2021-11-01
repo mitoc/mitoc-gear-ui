4075c01b87a9daa22628aa09d0c1b1a515c4e7d5
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-10-31 20:01:24 -0400

    Improve header

--- src/components/BaseLayout/Header.tsx
+++ src/components/BaseLayout/Header.tsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 export function Header() {
   return (
-    <nav className="navbar navbar-dark bg-dark navbar-fixed-top mb-4">
-      <div className="container">
-        <div className="navbar-header pull-left">
-          <Link className="navbar-brand" to="/">
-            MITOC Gear
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="dark" className="navbar-dark navbar-fixed-top mb-4">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          MITOC Gear
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/people">
+              People
+            </Nav.Link>
+            <Nav.Link as={Link} to="/gear">
+              Gear
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
