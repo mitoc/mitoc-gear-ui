c203117db92f4cbbd0aade94f2c1ee92bbb43c99
Florian Pagnoux <florian.pagnoux@gmail.com>
2022-03-27 18:04:03 -0400

    Fix layout

--- src/pages/People/BuyGear.tsx
+++ src/pages/People/BuyGear.tsx
@@ -10,33 +10,35 @@ type Props = {
 export function BuyGear({ onAdd }: Props) {
   const items = usePurchasableItems();
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Buy</th>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item) => {
-          const { name, price } = item;
-          return (
-            <tr>
-              <td className="text-center">
-                <button
-                  className="btn btn-outline-primary w-100 h-100"
-                  onClick={() => onAdd({ id: uniqueID(), item })}
-                >
-                  Buy
-                </button>
-              </td>
-              <td>{name}</td>
-              <td>{price}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="border rounded-2 p-2 bg-light">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Buy</th>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => {
+            const { name, price } = item;
+            return (
+              <tr>
+                <td className="text-center">
+                  <button
+                    className="btn btn-outline-primary w-100 h-100"
+                    onClick={() => onAdd({ id: uniqueID(), item })}
+                  >
+                    Buy
+                  </button>
+                </td>
+                <td>{name}</td>
+                <td>{price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
