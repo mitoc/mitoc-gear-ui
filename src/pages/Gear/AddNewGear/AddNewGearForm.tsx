d528b1b5ae9a786424b9b6dfdaaa561f6323dd60
Florian Pagnoux <florian.pagnoux@gmail.com>
2022-04-17 14:42:16 -0400

    Use react hook form in New gear form

--- src/pages/Gear/AddNewGear/AddNewGearForm.tsx
+++ src/pages/Gear/AddNewGear/AddNewGearForm.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
 import Select from "react-select";
+import { useForm } from "react-hook-form";
 
 import { useGetGearTypesQuery } from "features/api";
-import { Checkbox } from "components/Inputs/Checkbox";
-import { NumberField } from "components/Inputs/NumberField";
-import { TextField } from "components/Inputs/TextField";
-import { TextArea } from "components/Inputs/TextArea";
 import { CreateGearArgs, GearType } from "apiClient/gear";
 
 export function AddNewGearForm({
@@ -18,25 +15,40 @@ export function AddNewGearForm({
   const [gearType, setGearType] = useState<
     (GearType & { value: string; label: string }) | null
   >(null);
-  const [quantity, setQuantity] = useState<number | null>(1);
-  const [depositAmount, setDepositAmount] = useState<number | null>(null);
-  const [autoGenerateIds, setAutoGenerateIds] = useState<boolean>(true);
-  const [idSuffix, setIdSuffix] = useState<string>("");
-  const [spec, setSpec] = useState<string>("");
-  const [size, setSize] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
 
-  const handleSubmit = () => {
-    if (gearType == null || !quantity) {
-      return null;
+  const { register, handleSubmit: handleSubmit2, watch, setValue } = useForm({
+    defaultValues: {
+      specification: "",
+      size: "",
+      description: "",
+      idSuffix: "",
+      autoGenerateIds: true,
+      depositAmount: null,
+      quantity: 1,
+    },
+  });
+
+  const autoGenerateIds = watch("autoGenerateIds");
+  useEffect(() => {
+    if (autoGenerateIds) {
+      setValue("idSuffix", "");
     }
-    return onSubmit({
-      type: gearType.value,
-      quantity,
-      idSuffix,
-      specification: spec,
-      size,
-    });
+  }, [autoGenerateIds]);
+  useEffect(() => {
+    if (!autoGenerateIds && gearType) {
+      setValue("idSuffix", gearType.shorthand + "-");
+    }
+  }, [autoGenerateIds, gearType]);
+
+  const handleSubmit = (args: any) => {
+    console.log(args);
+    // return onSubmit({
+    //   type: gearType.value,
+    //   quantity,
+    //   idSuffix,
+    //   specification: spec,
+    //   size,
+    // });
   };
 
   const options =
@@ -46,14 +58,8 @@ export function AddNewGearForm({
       ...gearType,
     })) ?? [];
 
-  useEffect(() => {
-    if (autoGenerateIds) {
-      setIdSuffix("");
-    }
-  }, [autoGenerateIds]);
-
   return (
-    <form>
+    <form onSubmit={handleSubmit2(handleSubmit)}>
       <label className="w-100 mb-2">
         Gear type:
         <Select
@@ -65,55 +71,60 @@ export function AddNewGearForm({
       </label>
       <label className="w-100 mb-2">
         Quantity to add:
-        <NumberField integer={true} value={quantity} onChange={setQuantity} />
+        <input
+          type="number"
+          className="form-control"
+          {...register("quantity", { required: true, valueAsNumber: true })}
+        />
       </label>
       <label className="mb-2 form-switch">
-        <Checkbox value={autoGenerateIds} onChange={setAutoGenerateIds} />{" "}
+        <input
+          type="checkbox"
+          className="form-check-input"
+          {...register("autoGenerateIds")}
+        />{" "}
         Automatically generate new ids
       </label>
       {!autoGenerateIds && (
         <label className="mb-2 w-100">
           First ID:
-          {/* TODO: Validate this */}
-          <TextField
-            value={gearType ? gearType?.shorthand + "-" + idSuffix : ""}
-            onChange={(val) => {
-              const suffix = val.slice(3);
-              setIdSuffix(suffix);
-            }}
+          <input
+            type="text"
+            className="form-control"
+            {...register("idSuffix")}
           />
         </label>
       )}
       <label className="mb-2 w-100">
         Deposit amount:
-        <NumberField
-          className="w-100"
-          value={depositAmount}
-          onChange={setDepositAmount}
+        <input
+          type="number"
+          step={0.5}
+          className="form-control"
+          {...register("depositAmount", {
+            required: true,
+            valueAsNumber: true,
+          })}
         />
       </label>
       <label className="mb-2 w-100">
         Specification:
-        <TextField value={spec} onChange={setSpec} />
+        <input
+          type="text"
+          className="form-control"
+          {...register("specification")}
+        />
       </label>
       <label className="mb-2 w-100">
         Size:
-        <TextField value={size} onChange={setSize} />
+        <input type="text" className="form-control" {...register("size")} />
       </label>
       <label className="mb-2 w-100">
         Description:
-        <TextArea
-          className="w-100"
-          value={description}
-          onChange={setDescription}
-        />
+        <textarea className="form-control" {...register("description")} />
       </label>
       <div className="d-flex justify-content-end w-100">
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleSubmit}
-        >
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </div>
