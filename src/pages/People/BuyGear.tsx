a985cce6c826fa7e8b9d26aa2959557d6c6fdbf2
Florian Pagnoux <florian.pagnoux@gmail.com>
2022-12-26 11:54:15 +0100

    Add search for purcheasables

--- src/pages/People/BuyGear.tsx
+++ src/pages/People/BuyGear.tsx
@@ -1,15 +1,34 @@
 import { useGetPurchasablesQuery } from "redux/api";
+import { useState } from "react";
 
 import { fmtAmount } from "lib/fmtNumber";
 import { uniqueID } from "lib/uniqueID";
+import { SearchTextField } from "components/Inputs/TextField";
 
 import { usePersonPageContext } from "./PeoplePage/PersonPageContext";
 
 export function BuyGear() {
   const { data: items = [] } = useGetPurchasablesQuery();
   const { purchaseBasket } = usePersonPageContext();
+  const [query, setQuery] = useState<string>("");
   return (
     <div className="border rounded-2 p-2 bg-light">
+      <h3 className="mb-4">Buy gear</h3>
+      <label className="w-100 mb-2 d-flex flex-row align-items-center">
+        <SearchTextField
+          value={query}
+          onChange={(newQuery) => {
+            setQuery(newQuery);
+          }}
+          placeholder="Search"
+          debounceTime={300}
+          className="mb-3"
+          style={{
+            width: "250px",
+            maxWidth: "100%",
+          }}
+        />
+      </label>
       <table className="table">
         <thead>
           <tr>
@@ -19,23 +38,29 @@ export function BuyGear() {
           </tr>
         </thead>
         <tbody>
-          {items.map((item) => {
-            const { id, name, price } = item;
-            return (
-              <tr key={id}>
-                <td className="text-center">
-                  <button
-                    className="btn btn-outline-primary w-100 h-100"
-                    onClick={() => purchaseBasket.add({ id: uniqueID(), item })}
-                  >
-                    Buy
-                  </button>
-                </td>
-                <td>{name}</td>
-                <td>{fmtAmount(price)}</td>
-              </tr>
-            );
-          })}
+          {items
+            .filter((item) =>
+              item.name.toLowerCase().includes(query.toLowerCase())
+            )
+            .map((item) => {
+              const { id, name, price } = item;
+              return (
+                <tr key={id}>
+                  <td className="text-center">
+                    <button
+                      className="btn btn-outline-primary w-100 h-100"
+                      onClick={() =>
+                        purchaseBasket.add({ id: uniqueID(), item })
+                      }
+                    >
+                      Buy
+                    </button>
+                  </td>
+                  <td>{name}</td>
+                  <td>{fmtAmount(price)}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
