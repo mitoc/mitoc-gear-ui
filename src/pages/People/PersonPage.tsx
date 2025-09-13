619b2dbfb91afa5d5971d32f5aaba6fe7ba0cd6c
Florian Pagnoux <florian.pagnoux@tulip.co>
2025-09-12 21:20:08 -0400

    Add warning

--- src/pages/People/PersonPage.tsx
+++ src/pages/People/PersonPage.tsx
@@ -14,12 +14,12 @@ import {
   PersonPageContextProvider,
   usePersonPageContext,
 } from "./PeoplePage/PersonPageContext";
+import { PersonApprovals } from "./PersonApprovals";
 import { PersonProfile } from "./PersonProfile";
 import { PersonRentals } from "./PersonRentals";
 import { PersonRentalsHistory } from "./PersonRentalsHistory";
 import { PersonPageTabs, PersonTabsSelector } from "./PersonTabs";
 import { ReturnStaging } from "./ReturnStaging";
-import { PersonApprovals } from "./PersonApprovals";
 
 export function PersonPage() {
   const { personId } = useParams<{ personId: string }>();
@@ -52,17 +52,34 @@ function PersonPageInner() {
     checkoutBasket,
     returnBasket,
     purchaseBasket,
+    isApproved,
   } = usePersonPageContext();
 
   useSetPageTitle(person ? `${person.firstName} ${person.lastName} ` : "");
 
   const isOverdue = person.rentals.some((rental) => rental.weeksOut >= 7);
 
+  const unaprovedRestrictedItems = checkoutBasket.items.filter(
+    ({ id, type }) => !isApproved(id, type.id),
+  );
+
   return (
     <div className="row">
-      {isOverdue && (
+      {isOverdue && isEmpty(unaprovedRestrictedItems) && (
         <div className="alert alert-danger" role="alert">
           Please remind renter to return overdue gear!
+        </div>
+      )}
+      {!isEmpty(unaprovedRestrictedItems) && (
+        <div className="alert alert-warning" role="alert">
+          ⚠️ <strong>Unapproved restricted gear</strong> in the basket:
+          <ul>
+            {unaprovedRestrictedItems.map((i) => (
+              <li key={i.id}>{i.id}</li>
+            ))}
+          </ul>
+          Please ensure that the renter has been approved by the relevant
+          activity chair to check this gear out (i.e. via email)
         </div>
       )}
       <div className="col-12 col-md-5 p-2">
